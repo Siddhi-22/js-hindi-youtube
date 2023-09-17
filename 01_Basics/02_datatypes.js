3b0cdc4424b7ebf69672d137e3100a85cee79150
Siddhi-22 <maharanasiddhi@gmail.com>
2023-09-17 10:23:41 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "Siddhi";
+let age = 25;
+let login = true;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stand-alone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "Sid");
