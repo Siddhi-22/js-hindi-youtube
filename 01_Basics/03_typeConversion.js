a82c4be91e2636c70d463ff5dca944aa29ea2b2d
Siddhi-22 <maharanasiddhi@gmail.com>
2023-09-18 19:46:58 +0530

    typeConversion

--- 01_Basics/03_typeConversion.js
+++ 01_Basics/03_typeConversion.js
@@ -1,3 +1,24 @@
-let score = 39;
+//let score = "39as"; //NaN
+//let score = null //object
+//let score = undefined; 
+//let score = true;   // true=1, false=0
+let score = "Siddhi";
 
-console.log(typeof score);
+
+ console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//let logedIn = 1; //true
+//let logedIn = ""; //false
+//let logedIn = "Siddhi"; //true
+ let booleanLogedIn = Boolean(logedIn);
+ console.log(booleanLogedIn);
+
+let num = 45;
+let stringNum = String(num);
+console.log(stringNum);
+console.log(typeof stringNum);
