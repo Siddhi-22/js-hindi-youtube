08264189f009d238f848506799fd4f802dba01e9
Siddhi-22 <maharanasiddhi@gmail.com>
2023-09-21 23:41:59 +0530

    comparisions

---
+++ 01_Basics/05_Comparisions.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(1 <= 2);
+// console.log(2 != 3);
+// console.log(1 == 3);
+
+
+// console.log("2" == 2);
+// console.log(1 == "3");
+
+/* 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 0);
+ 
+// The reason is that an equality check == and comparisions (<, >, <=, >=)
+// work differently
+// Comparisions converts null>=0 to a number, treating it as 0
+// Therefore (12) (null>=0) is true          
+*/
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined < 0);
+
+
+console.log(2 === "2");
+
