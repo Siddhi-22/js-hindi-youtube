fe4d2e2c26ebeb6d058a99765ed3e393266878f5
Siddhi-22 <maharanasiddhi@gmail.com>
2023-09-21 21:11:16 +0530

    changes

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,22 @@
+// Operations
+
+
+// let value = 7;
+// let negvalue = -value;
+// console.log(negvalue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 3);
+
+let str1 = "Hello ", str2 = "Siddhi";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("2" + 4);
+console.log(2 + "4");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
