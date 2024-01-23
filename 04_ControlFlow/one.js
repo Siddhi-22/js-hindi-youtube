f1c7116e378708d9a3873aaa6b188513e65219fd
Siddhi-22 <maharanasiddhi@gmail.com>
2024-01-23 11:40:39 +0530

    new folder

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,10 @@
+// if 
+
+
+if (2 !== 3) {
+    const power ="fly";
+    console.log(`User has the power to ${power}`);
+}else{
+    console.log(`that was not true....`)
+};
+// <, >, <=, >=, ==, !=, ===, !== 
