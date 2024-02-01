4d5df2c3f7283e91ccffb92f47846d9002f06f17
Siddhi-22 <maharanasiddhi@gmail.com>
2024-02-01 12:44:24 +0530

    add

---
+++ 06_DOM/01_first.js
@@ -0,0 +1,6 @@
+//const element = document.getElementById('title');
+//element.style.backgroundColor = 'green';
+
+
+const changed = document.getElementsByClassName('heading')
+changed.style.backgroundColor = 'yellow';
