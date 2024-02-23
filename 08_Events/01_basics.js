9c552d4ada2cb98e3e3cfde6b0c98af71e3787ba
Siddhi-22 <maharanasiddhi@gmail.com>
2024-02-23 09:03:51 +0530

    new added changes

--- 08_Events/01_basics.js
+++ 08_Events/01_basics.js
@@ -6,16 +6,27 @@
 //Event Bubbling(false)
 // bottom-up event propagation
 
-document.getElementById('images').addEventListener('click', function(e){
-    console.log("Clicked inside ul");
-}, false);
+// document.getElementById('images').addEventListener('click', function(e){
+//     console.log("Clicked inside ul");
+// }, false);
 
 
 
 //Event Capturing(true)
 //top-down event propagation
 
-document.getElementById('mirror').addEventListener('click', function(e){
-    console.log("Mirror Clicked..");
-    e.stopPropagation(); //this will prevent propagation to higher elements.
-}, false);
+// document.getElementById('mirror').addEventListener('click', function(e){
+//     console.log("Mirror Clicked..");
+//     e.stopPropagation(); //this will prevent propagation to higher elements.
+// }, false);
+
+
+document.querySelector('#images').addEventListener('click', (e) => {
+    console.log(e.target.parentNode);
+
+    if (e.target.tagName === 'IMG') {
+        let removeIt = e.target.parentNode;
+        removeIt.remove();
+        removeIt.parentNode.removeChild(removeIt);
+    }
+}, false)
